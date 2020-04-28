4b8877e6cf932ab7cc792b3151627e15c78bbf18
Patrick Charles <patrickrcharles@gmail.com>
2020-04-27 19:11:00 -0500

    added gameoption class

---
+++ Assets/Scripts/level_start/GameOptions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public  class GameOptions : MonoBehaviour
+{
+    [SerializeField]
+    static public String playerSelected;
+    [SerializeField]
+    static public String levelSected;
+    static public int gameModeSelected;
+
+    static public void printCurrentValues()
+    {
+        Debug.Log("playerSelected : "+ playerSelected );
+        Debug.Log("levelSected : " + levelSected);
+        Debug.Log("gameModeSelected : " + gameModeSelected);
+    }
+}
