10e168199b5da93a911b85467b2e4b4aba6c3ce3
Patrick Charles <patrickrcharles@gmail.com>
2020-05-02 12:14:51 -0500

    added screenshoting of editor and fps display

--- Assets/Scripts/FPSDisplay.cs
+++ Assets/Scripts/FPSDisplay.cs
@@ -16,9 +16,9 @@ public class FPSDisplay : MonoBehaviour
 
         GUIStyle style = new GUIStyle();
 
-        Rect rect = new Rect(0, 0, w, h * 2 / 100);
+        Rect rect = new Rect(0, 0, w, h * 2 / 50);
         style.alignment = TextAnchor.UpperLeft;
-        style.fontSize = h * 2 / 100;
+        style.fontSize = h * 2 / 50;
         style.normal.textColor = new Color(1.0f, 1.0f, 1.0f, 1.0f);
         float msec = deltaTime * 1000.0f;
         float fps = 1.0f / deltaTime;
