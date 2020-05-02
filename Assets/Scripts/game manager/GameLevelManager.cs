b64328e92c329aab8fd3231c020ae6a91f67b57d
Patrick Charles <patrickrcharles@gmail.com>
2020-05-02 18:02:06 -0500

    working on creating game rules for game modes

---
+++ Assets/Scripts/game manager/GameLevelManager.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TeamUtility.IO;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class GameLevelManager : MonoBehaviour
+{
+    // this is to keep a reference to player in game manager 
+    // that can be retrieved across all scripts
+
+    [SerializeField]
+    GameObject _player;
+    private PlayerController _playerState;
+    private Animator _anim;
+
+    Vector3 previousPlayerPosition;
+    Quaternion previousPlayerRotation;
+
+    bool gameOver;
+    bool startGame;
+    bool locked;
+    bool paused;
+    //bool reloadLevel;
+    //bool showScore;
+
+    string currentSceneName;
+    //[SerializeField]
+    //GameObject startMenuMusicObject;
+
+    //[SerializeField]
+    //float totalMoney;
+
+    //public bool playerIsEnemy;
+    //public Text displayPlayerLives;
+    //public GameObject backgroundFade;
+    //public GameObject pauseObject;
+
+    //private AudioSource[] allAudioSources;
+
+    //BasketBall objects
+    BasketBall _basketballState;
+    GameObject _basketball;
+    GameObject basketballPrefab;
+
+
+    //spawn locations
+    GameObject basketballSpawnLocation;
+    GameObject playerSpawnLocation;
+    //[SerializeField]
+    //GameObject player_spawn;
+
+    [SerializeField]
+    private int playerCount;
+
+    [SerializeField] private int basketballCount;
+
+    public static GameLevelManager instance;
+
+    private GameObject playerClone;
+
+    //List<string> scenes = new List<string>();
+
+    [SerializeField]
+    private GameObject flashObject;
+
+    void Awake()
+    {
+        // initialize game manger player references
+        instance = this;
+
+        // if player selected is not null / player not selecetd
+        if(!string.IsNullOrEmpty( GameOptions.playerSelected)){
+            string playerPrefabPath = "Prefabs/characters/players/player_" + GameOptions.playerSelected.ToString();
+            playerClone = Resources.Load("Prefabs/characters/players/player_" + GameOptions.playerSelected.ToString()) as GameObject;
+        }
+        //allAudioSources = FindObjectsOfType<AudioSource>();
+        //Application.targetFrameRate = 60;
+
+        if (!getCurrentSceneName().StartsWith("start")) { InitializePlayer(); }
+        // player + ball spawn locations
+
+        playerSpawnLocation = GameObject.Find("player_spawn_location");
+        basketballSpawnLocation = GameObject.Find("ball_spawn_location");
+
+        if (GameObject.FindWithTag("Player") == null)
+        {
+            Instantiate(playerClone, playerSpawnLocation.transform.position, Quaternion.identity);
+        }
+        if(GameObject.FindWithTag("basketball") == null)
+        {
+            basketballPrefab = Resources.Load("Prefabs/objects/basketball_nba") as GameObject;
+            Instantiate(basketballPrefab, basketballSpawnLocation.transform.position, Quaternion.identity);
+        }
+
+
+    }
+
+    void Start()
+    {
+        locked = false;
+
+        //set up player/basketball read only references for use in other classes
+
+        _player = GameObject.FindGameObjectWithTag("Player");
+        _playerState = Player.GetComponent<PlayerController>();
+        _anim = Player.GetComponentInChildren<Animator>();
+        _basketball = GameObject.FindWithTag("basketball");
+        _basketballState = _basketball.GetComponent<BasketBall>();
+
+        InitializePlayer();
+
+        // if flash is selected player, disable flash auto player
+        flashObject = GameObject.Find("flash_auto_play");
+        if ( !string.IsNullOrEmpty(GameOptions.playerSelected) 
+             &&GameOptions.playerSelected.Contains("flash") 
+             && flashObject != null )
+        {
+            flashObject.SetActive(false);
+        }
+        GameOptions.printCurrentValues();
+    }
+
+
+    void Update()
+    {
+        //close app
+        if ((InputManager.GetKey(KeyCode.LeftShift) || InputManager.GetKey(KeyCode.RightShift)) 
+            && InputManager.GetKeyDown(KeyCode.Q))
+        {
+            Quit();
+        }
+
+        //reload start
+        if ((InputManager.GetKey(KeyCode.LeftShift) || InputManager.GetKey(KeyCode.RightShift))
+            && InputManager.GetKeyDown(KeyCode.P))
+        {
+            SceneManager.LoadScene("level_00_start");
+        }
+
+        //pause ESC, submit, cancel
+        if (InputManager.GetButtonDown("Submit")
+            || InputManager.GetButtonDown("Cancel")
+            || InputManager.GetKeyDown(KeyCode.Escape))
+        {
+            paused = TogglePause();
+        }
+        // reload scene 4+2+0
+        if (InputManager.GetKey(KeyCode.Alpha4)
+            && InputManager.GetKey(KeyCode.Alpha2)
+            && InputManager.GetKey(KeyCode.Alpha0)
+            && !locked)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        //turn off accuracy modifer 6+9
+        if (InputManager.GetKey(KeyCode.Alpha6)
+            && InputManager.GetKeyDown(KeyCode.Alpha9)
+            && !locked)
+        {
+            locked = true;
+            _basketballState.toggleAddAccuracyModifier();
+            locked = false;
+        }
+
+        //turn off accuracy modifer 6+9
+        if (InputManager.GetKey(KeyCode.LeftShift)
+            && InputManager.GetKeyDown(KeyCode.Alpha0)
+            && !locked)
+        {
+            locked = true;
+            _basketballState.testConclusions.getDataFromList();
+            _basketballState.testConclusions.printConclusions();
+            locked = false;
+        }
+    }
+
+    // set up player references that other scripts use
+    // game manager provides read only links to player object and player states
+    public void InitializePlayer()
+    {
+        Debug.Log("initialize player");
+        //_player = GameObject.FindGameObjectWithTag("Player");
+        //_playerState = player.GetComponent<playercontrollerscript>();
+        //_anim = player.GetComponentInChildren<Animator>();
+    }
+
+
+    private string getCurrentSceneName()
+    {
+        return SceneManager.GetActiveScene().name;
+    }
+
+    bool TogglePause()
+    {
+        if (Time.timeScale == 0f)
+        {
+            //gameManager.instance.backgroundFade.SetActive(false);
+            Time.timeScale = 1f;
+            //resumeAllAudio();
+            return (false);
+        }
+        else
+        {
+            //gameManager.instance.backgroundFade.SetActive(true);
+            Time.timeScale = 0f;
+            //pauseAllAudio();
+            return (true);
+        }
+    }
+
+    //void pauseAllAudio()
+    //{
+    //    foreach (AudioSource audioS in allAudioSources)
+    //    {
+    //        //audioS.Stop();
+    //        audioS.Pause();
+    //    }
+    //}
+
+    //void resumeAllAudio()
+    //{
+    //    foreach (AudioSource audioS in allAudioSources)
+    //    {
+    //        //audioS.Stop();
+    //        audioS.UnPause();
+    //    }
+    //}
+
+    private void Quit()
+    {
+        Application.Quit();
+    }
+
+    public GameObject Player
+    {
+        get { return _player; }
+    }
+    public PlayerController PlayerState
+    {
+        get { return _playerState; }
+    }
+    public Animator Anim
+    {
+        get { return _anim; }
+    }
+
+    public BasketBall BasketballState
+    {
+        get => _basketballState;
+    }
+
+    public GameObject Basketball
+    {
+        get => _basketball;
+    }
+}
---
+++ Assets/Scripts/game manager/GameRules.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TeamUtility.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameRules : MonoBehaviour
+{
+    private int playerId;
+    private int levelId;
+    [SerializeField]
+    private int gameModeId;
+    private float timerStart;
+
+    private bool gameMode1;
+    private bool gameMode2;
+    private bool gameMode3;
+    private bool gameMode4;
+
+    private bool gameOver;
+    private bool gameStart;
+
+    [SerializeField]
+    private float timeStart;
+    private Timer timer;
+    [SerializeField]
+    private BasketBallStats basketBallStats;
+
+    // object name that displays score
+    private const string displayScoreObjectName = "display_score";
+    [SerializeField]
+    private Text displayScoreText;
+
+    void Start()
+    {
+        gameModeId = 1;
+        //gameModeId = GameOptions.gameModeSelected;
+        timer = GameObject.Find("timer").GetComponent<Timer>();
+        setTimer(timeStart);
+
+        basketBallStats = BasketBall.instance.BasketBallStats;
+        displayScoreText = GameObject.Find(displayScoreObjectName).GetComponent<Text>();
+        displayScoreText.text = "";
+    }
+
+    void Update()
+    {
+        // if game is over,
+        // get stats for display
+        //if (gameOver)
+        //{
+        //    displayScore(gameModeId);
+        //}
+
+        //turn off accuracy modifer 6+9
+        if (InputManager.GetKey(KeyCode.LeftShift)
+            && InputManager.GetKeyDown(KeyCode.Alpha7))
+        {
+            displayScore(gameModeId);
+        }
+    }
+
+    private void displayScore(int modeId)
+    {
+        displayScoreText.text = getDisplayText(modeId);
+
+    }
+
+    private string getDisplayText(int modeId)
+    {
+        string displayText = "";
+        string statsSummary = "";
+
+        Debug.Log("getDisplayText()");
+        Debug.Log("game mode id " + gameModeId);
+        if (gameModeId == 1)
+        {
+
+            displayText = "You scored " + basketBallStats.TotalPoints + " total points\n\n" + getStatsTotals();
+        }
+
+        return displayText;
+    }
+
+    string getStatsTotals()
+    {
+        Debug.Log("getStatsTotals()");
+        string scoreText = "";
+        scoreText = "shots  : " + basketBallStats.ShotMade + " / " + basketBallStats.ShotAttempt + "\n"
+                         + "accuracy : " + BasketBall.instance.getTotalPointAccuracy() + "%\n"
+                         + "points : " + basketBallStats.TotalPoints + "\n"
+                         + "2 pointers : " + basketBallStats.TwoPointerMade + " / " + basketBallStats.TwoPointerAttempts + " accuracy : " + BasketBall.instance.getTwoPointAccuracy() + "%\n"
+                         + "3 pointers : " + basketBallStats.ThreePointerMade + " / " + basketBallStats.ThreePointerAttempts + " accuracy : " + BasketBall.instance.getThreePointAccuracy() + "%\n"
+                         + "4 pointers : " + basketBallStats.FourPointerMade + " / " + basketBallStats.FourPointerAttempts + " accuracy : " + BasketBall.instance.getFourPointAccuracy() + "%\n"
+                         + "longest shot distance : " + (Math.Round(basketBallStats.LongestShotMade, 2) * 6f).ToString("0.00") + " ft.";
+
+        Debug.Log("scoreText : " + scoreText);
+
+        return scoreText;
+    }
+
+    private void setTimer(float seconds)
+    {
+        timer.TimeStart = seconds;
+    }
+
+
+}
