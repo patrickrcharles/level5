977413251ed435618e465bd6dc6d393f059d191a
Patrick Charles <patrickrcharles@gmail.com>
2020-07-01 16:37:26 -0500

    achievement unlock logic + unlock notification

--- Assets/Scripts/achievements/Achievement.cs
+++ Assets/Scripts/achievements/Achievement.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Net;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Achievement : MonoBehaviour
 {
@@ -21,6 +22,8 @@ public class Achievement : MonoBehaviour
     [SerializeField]
     bool modeUnlock;
     [SerializeField]
+    bool otherUnlock;
+    [SerializeField]
     private int playerId;
     [SerializeField]
     private int cheerleaderId;
@@ -33,12 +36,20 @@ public class Achievement : MonoBehaviour
     private int activationValueInt;
     [SerializeField]
     private float activationValueFloat;
+    [SerializeField]
+    private int activationValueProgressionInt;
+    [SerializeField]
+    private float activationValueProgressionFloat;
 
     // other custom 
     [SerializeField]
     bool consecutive;
     [SerializeField]
     bool count;
+    [SerializeField]
+    bool isProgressiveCount;
+    [SerializeField]
+    bool isProgressiveTime;
     [SerializeField]
     bool time;
     [SerializeField]
@@ -49,6 +60,8 @@ public class Achievement : MonoBehaviour
     bool seven;
     [SerializeField]
     private int playerRequiredToUseId;
+    [SerializeField]
+    private int cheerleaderRequiredToUseId;
     [SerializeField]
     private int levelRequiredToUseId;
     [SerializeField]
@@ -67,97 +80,124 @@ public class Achievement : MonoBehaviour
     public int PlayerId { get => playerId; set => playerId = value; }
     public bool IsLocked { get => isLocked; set => isLocked = value; }
     public string AchievementDescription { get => achievementDescription; set => achievementDescription = value; }
+    public int CheerleaderId { get => cheerleaderId; set => cheerleaderId = value; }
 
-    public void checkCharacterUnlockConditions(int pid, int lid, int mid, int activateValue)
+    // new achievement object
+    public Achievement(int aid, int islockedValue)
     {
-        bool condition1 = false;
-        bool condition2 = false;
-        bool condition3 = false;
-
-        // if unlocks character and requires counter
-        if (characterUnlock && count && IsLocked)
+        achievementId = aid;
+        if (islockedValue == 1)
         {
-            //Debug.Log("aid: " + achievementId + " pid = " + pid + " lid : " + lid + " mid :" + mid + " " + activationValueInt + " :" + activateValue);
-            //Debug.Log("unlockable : " + achievementName);
-
-            if (playerRequiredToUseId == 0 && checkActivateValue(activateValue))
-            {
-                condition1 = true;
-            }
-            if (levelRequiredToUseId == 0 && checkActivateValue(activateValue))
-            {
-                condition2 = true;
-            }
-            if (modeRequiredToUseId == 0 && checkActivateValue(activateValue))
-            {
-                condition3 = true;
-            }
-            // != 0 is check to see achievement needspid, lid, or mid
-            if (pid == playerRequiredToUseId && checkActivateValue(activateValue) && !condition1)
-            {
-                condition1 = true;
-            }          
-            if(lid == levelRequiredToUseId && checkActivateValue(activateValue) && !condition2)
-            {
-                condition2 = true;
-            }
-            if (mid == modeRequiredToUseId && checkActivateValue(activateValue) && !condition3)
-            {
-                condition3 = true;
-            }
-            if (condition1 && condition2 && condition3)
-            {
-                IsLocked = false;
-                messageLog.instance.toggleMessageDisplay("character id: " + PlayerId + " unlocked ");
-                //Debug.Log("====================================  character id: " + playerId + " unlocked ");
-            }
+            IsLocked = true;
+        }
+        if (islockedValue == 0)
+        {
+            IsLocked = false;
         }
     }
 
-    public void checkCheerleaderUnlockConditions(int cid, int lid, int mid, int activateValue)
+    public void checkUnlockConditions(int playid, int cheerid, int lid, int mid, int activateValue, BasketBallStats basketBallStats)
     {
         bool condition1 = false;
         bool condition2 = false;
         bool condition3 = false;
 
-        // if unlocks character and requires counter
-        if (cheerleaderUnlock && count && IsLocked)
+        // if unlocks character and islocked
+        if (IsLocked && characterUnlock)
         {
-            //Debug.Log("aid: " + achievementId + " pid = " + pid + " lid : " + lid + " mid :" + mid + " " + activationValueInt + " :" + activateValue);
-            //Debug.Log("unlockable : " + achievementName);
-
-            if (playerRequiredToUseId == 0 && checkActivateValue(activateValue))
+            if (count && !isProgressiveCount)
             {
-                condition1 = true;
+                if (checkPlayerUnlockConditions(playerId, lid, mid, ref condition1, ref condition2, ref condition3)
+                    && checkActivateValue(activateValue))
+                {
+                    isLocked = false;
+                }
             }
-            if (levelRequiredToUseId == 0 && checkActivateValue(activateValue))
+            // other unlock : if requires count but is progressive counter
+            if (count && isProgressiveCount)
             {
-                condition2 = true;
-            }
-            if (modeRequiredToUseId == 0 && checkActivateValue(activateValue))
-            {
-                condition3 = true;
-            }
-            // != 0 is check to see achievement needspid, lid, or mid
-            if (cid == playerRequiredToUseId && checkActivateValue(activateValue) && !condition1)
-            {
-                condition1 = true;
-            }
-            if (lid == levelRequiredToUseId && checkActivateValue(activateValue) && !condition2)
-            {
-                condition2 = true;
-            }
-            if (mid == modeRequiredToUseId && checkActivateValue(activateValue) && !condition3)
-            {
-                condition3 = true;
-            }
-            if (condition1 && condition2 && condition3)
-            {
-                IsLocked = false;
-                messageLog.instance.toggleMessageDisplay("character id: " + PlayerId + " unlocked ");
-                //Debug.Log("====================================  character id: " + playerId + " unlocked ");
+                if (checkPlayerUnlockConditions(playerId, lid, mid, ref condition1, ref condition2, ref condition3))            
+                {
+                    // if progressive activate value reached
+                    if (checkActivateValueProgressive(activateValue))
+                    {
+                        isLocked = false;
+                    }
+                    // else, update progression
+                    else
+                    {
+                        activationValueProgressionInt += activateValue;
+                    }
+                }
             }
         }
+        // if unlocks cheerleader and islocked
+        if (IsLocked && cheerleaderUnlock)
+        {
+            // cheerleader unlock :  if requires count but isnt progressive counter
+            if (count && !isProgressiveCount)
+            {
+                if (checkCheerleaderUnlockConditions(cheerid, lid, mid, ref condition1, ref condition2, ref condition3)
+                    && checkActivateValue(activateValue))
+                {
+                    IsLocked = false;
+                }
+            }
+            // cheerleader unlock : if requires count but is progressive counter
+            if (count && isProgressiveCount)
+            {
+                // if conditions met and value met :: unlock
+                if (checkCheerleaderUnlockConditions(cheerid, lid, mid, ref condition1, ref condition2, ref condition3))
+                {
+                    // if progressive activate value reached
+                    if (checkActivateValueProgressive(activateValue))
+                    {
+                        isLocked = false;
+                    }
+                    // else, update progression
+                    else
+                    {
+                        activationValueProgressionInt += activateValue;
+                    }
+                }
+            }
+        }
+        // if unlocks other (anything)
+        if (IsLocked && otherUnlock)
+        {
+            // cheerleader unlock :  if requires count but isnt progressive counter
+            if (count && !isProgressiveCount)
+            {
+                if (checkPlayerUnlockConditions(playerId, lid, mid, ref condition1, ref condition2, ref condition3)
+                    && checkActivateValue(activateValue))
+                {
+                    isLocked = false;
+                }
+            }
+            // other unlock : if requires count but is progressive counter
+            if (count && isProgressiveCount)
+            {
+                // if conditions met
+                if (checkPlayerUnlockConditions(playerId, lid, mid, ref condition1, ref condition2, ref condition3))
+                {
+                    // if progressive activate value reached
+                    if (checkActivateValueProgressive(activateValue))
+                    {
+                        isLocked = false;
+                    }
+                    // else, update progression
+                    else
+                    {
+                        activationValueProgressionInt += activateValue;
+                    }
+                }
+            }
+        }
+        //// if achievement unlocked, display notification
+        //if(isLocked == false)
+        //{
+        //    StartCoroutine(displayAchievementUnlockNotification(3));
+        //}
     }
 
     public bool checkActivateValue(int activateValue)
@@ -166,7 +206,7 @@ public class Achievement : MonoBehaviour
 
         if (EQUAL_TO)
         {
-            if(activateValue == activationValueInt)
+            if (activateValue == activationValueInt)
             {
                 activate = true;
             }
@@ -180,13 +220,131 @@ public class Achievement : MonoBehaviour
         }
         if (GREATER_THAN)
         {
-            //Debug.Log(activate + " > " + activationValueInt);
             if (activateValue >= activationValueInt)
+            {
+                activate = true;
+                activationValueProgressionInt += activateValue;
+            }
+            else
+            {
+                activate = false;
+            }
+        }
+
+        return activate;
+    }
+    // for adding total count
+    public bool checkActivateValueProgressive(int activateValue)
+    {
+        bool activate = false;
+        int totalValue = activateValue + activationValueProgressionInt;
+
+        if (LESS_THAN)
+        {
+            if (totalValue < activationValueInt)
             {
                 activate = true;
             }
         }
-        //Debug.Log("activate : " + activate);
+        if (GREATER_THAN)
+        {
+            if (totalValue >= activationValueInt)
+            {
+                activate = true;
+                activationValueProgressionInt += activateValue;
+            }
+            // if less than activation, add to progressive value
+            if (totalValue < activationValueInt)
+            {
+                activate = false;
+            }
+        }
         return activate;
+    }
+    // for adding total time
+    public bool checkActivateValueProgressive(float activateValue)
+    {
+        bool activate = false;
+        float totalValue = activateValue + activationValueProgressionFloat;
+        if (GREATER_THAN)
+        {
+            if (totalValue >= activationValueInt)
+            {
+                activate = true;
+            }
+        }
+        return activate;
+    }
+
+    private bool checkPlayerUnlockConditions(int pid, int lid, int mid, ref bool condition1, ref bool condition2, ref bool condition3)
+    {
+        if (playerRequiredToUseId == 0)
+        {
+            condition1 = true;
+        }
+        if (levelRequiredToUseId == 0)
+        {
+            condition2 = true;
+        }
+        if (modeRequiredToUseId == 0)
+        {
+            condition3 = true;
+        }
+        // if requires specific player, this points to a pid
+        if (pid == playerRequiredToUseId && !condition1 && characterUnlock)
+        {
+            condition1 = true;
+        }
+        // if requires specific cheerleader, this points to a cid
+        if (pid == cheerleaderRequiredToUseId && !condition1 && characterUnlock)
+        {
+            condition1 = true;
+        }
+
+        if (lid == levelRequiredToUseId && !condition2)
+        {
+            condition2 = true;
+        }
+        if (mid == modeRequiredToUseId && !condition3)
+        {
+            condition3 = true;
+        }
+        if (condition1 && condition2 && condition3)
+        {
+            return true;
+        }
+        return false;
+    }
+    private bool checkCheerleaderUnlockConditions(int cid, int lid, int mid, ref bool condition1, ref bool condition2, ref bool condition3)
+    {
+        if (cheerleaderRequiredToUseId == 0)
+        {
+            condition1 = true;
+        }
+        if (levelRequiredToUseId == 0)
+        {
+            condition2 = true;
+        }
+        if (modeRequiredToUseId == 0)
+        {
+            condition3 = true;
+        }
+        if (cid == cheerleaderRequiredToUseId && !condition1 && cheerleaderUnlock)
+        {
+            condition1 = true;
+        }
+        if (lid == levelRequiredToUseId && !condition2)
+        {
+            condition2 = true;
+        }
+        if (mid == modeRequiredToUseId && !condition3)
+        {
+            condition3 = true;
+        }
+        if (condition1 && condition2 && condition3)
+        {
+            return true;
+        }
+        return false;
     }
 }
--- Assets/Scripts/achievements/AchievementManager.cs
+++ Assets/Scripts/achievements/AchievementManager.cs
@@ -2,12 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.InputSystem.Editor;
+using UnityEngine.UI;
 
 public class AchievementManager : MonoBehaviour
 {
     private List<Achievement> achievementList; // dictionary of achievements
-    public List<Achievement> AchievementList { get => achievementList; set => achievementList = value; }
-    public bool ListCreated { get => listCreated; set => listCreated = value; }
+
+    const string notificationObjectName = "achievement_notification_object";
+    const string notificationTextObjectName = "achievement_text";
+    const string notificationImageObjectName = "achievement_background";
+
+    [SerializeField]
+    GameObject notificationObject;
+    Text notificationObjectText;
+    Image notificationObjectImage;
 
     public static AchievementManager instance;
     bool listCreated;
@@ -28,6 +37,13 @@ public class AchievementManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        NotificationObject = GameObject.Find(notificationObjectName);
+        NotificationObjectText = GameObject.Find(notificationTextObjectName).GetComponent<Text>();
+        notificationObjectImage = GameObject.Find(notificationImageObjectName).GetComponent<Image>();
+
+        NotificationObject.SetActive(false);
+
         loadAchievements();
     }
 
@@ -46,11 +62,39 @@ public class AchievementManager : MonoBehaviour
         ListCreated = true;
     }
 
-    public void checkAllAchievements(int pid, int lid, int mid, int activateValue)
+    public void checkAllAchievements(int pid, int cid, int lid, int mid, int activateValue, BasketBallStats basketBallStats)
     {
         foreach (Achievement ach in AchievementList)
         {
-            ach.checkCharacterUnlockConditions(pid, lid, mid, activateValue);
+            ach.checkUnlockConditions(pid, cid, lid, mid, activateValue, basketBallStats);
+            if (!ach.IsLocked)
+            {
+                Debug.Log(" unlock achievement");
+                StartCoroutine(displayAchievementUnlockNotification(5, ach.achievementName));
+            }
         }
     }
+
+    public IEnumerator displayAchievementUnlockNotification(float seconds, string achName)
+    {
+        //GameObject messageObject = AchievementManager.instance.NotificationObject;
+        //Text messageText = AchievementManager.instance.NotificationObjectText;
+        //Image messageText = AchievementManager.instance.NotificationObjectText;
+
+        notificationObject.SetActive(true);
+        yield return new WaitUntil(() => notificationObject.activeInHierarchy == true);
+
+        notificationObjectText.text = "Achievement unlocked : \n" + achName;
+
+        yield return new WaitForSecondsRealtime(seconds);
+
+        notificationObjectText.text = "";
+        notificationObject.SetActive(false);
+
+    }
+
+    public GameObject NotificationObject { get => notificationObject; set => notificationObject = value; }
+    public Text NotificationObjectText { get => notificationObjectText; set => notificationObjectText = value; }
+    public List<Achievement> AchievementList { get => achievementList; set => achievementList = value; }
+    public bool ListCreated { get => listCreated; set => listCreated = value; }
 }
