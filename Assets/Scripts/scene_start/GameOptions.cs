5b43c96be9c69e50f2aa66b9b7cf2b586a223802
Patrick Charles <patrickrcharles@gmail.com>
2020-07-12 19:58:57 -0500

    renamed start scene folder

---
+++ Assets/Scripts/scene_start/GameOptions.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public  class GameOptions : MonoBehaviour
+{
+    static public string applicationVersion;
+    static public string operatingSystemVersion;
+
+    static public String playerDisplayName;
+    static public String cheerleaderDisplayName;
+    static public int playerId;
+    static public String levelSelected;
+    static public int gameModeSelected;
+    static public int levelId;
+    static public int cheerleaderId;
+
+    static public String playerObjectName;
+    static public String cheerleaderObjectName;
+    static public String levelSelectedName;
+    static public String levelDisplayName;
+    static public String gameModeSelectedName;
+    static public String cheerleaderSelectedName;
+
+    static public int levelSelectedRootCount;
+    static public int levelSelectedBuildIndex;
+
+
+    //player stats
+    static public float accuracy2pt;
+    static public float accuracy3pt;
+    static public float accuracy4pt;
+    static public float accuracy7pt;
+
+    static public float jumpForce;
+    static public float speed;
+    static public float runSpeed;
+    static public float runSpeedHasBall;
+    //static public float hangTime;
+   // static public float range;
+   // static public float release;
+    static public float criticalPercent;
+    static public float shootAngle;
+
+    // for testing scenes. True if scene loaded from start screen
+    static public bool gameModeHasBeenSelected;
+    static public bool gameModeRequiresCounter;
+    static public bool gameModeRequiresCountDown;
+
+    static public bool gameModeRequiresShotMarkers3s;
+    static public bool gameModeRequiresShotMarkers4s;
+
+    static public bool gameModeRequiresMoneyBall;
+
+    static public bool gameModeRequiresConsecutiveShot;
+
+    static public bool trafficEnabled;
+
+    //static public int level;
+    //static public int experience;
+    //static public decimal money;
+
+    void Awake()
+    {
+        levelSelectedRootCount = SceneManager.GetActiveScene().rootCount;
+        levelSelectedName = SceneManager.GetActiveScene().name;
+        levelSelectedBuildIndex = SceneManager.GetActiveScene().buildIndex;
+
+        applicationVersion = Application.version;
+
+        //gameModeSelected = 8;
+
+        //gameModeRequiresCounter = true;
+
+        //gameModeRequiresShotMarkers3s = true;
+        //gameModeRequiresShotMarkers4s = true;
+
+        //Debug.Log("levelSected : " + levelSelected);
+        //Debug.Log("levelSectedName : " + levelSelectedName);
+        //Debug.Log("levelSelectedBuildIndex : " + levelSelectedBuildIndex);
+    }
+
+    static public void printCurrentValues()
+    {
+        //Debug.Log("playerSelected : " + playerId);
+        //Debug.Log("levelSected : " + levelSelectedName);
+        //Debug.Log("gameModeSelected : " + gameModeSelected);
+        //Debug.Log("gameModeSelectedName : " + gameModeSelectedName);
+        //Debug.Log("gameModeHasBeenSelected : " + gameModeHasBeenSelected);
+        //Debug.Log("3s : " + gameModeRequiresShotMarkers3s);
+        //Debug.Log("4x : " + gameModeRequiresShotMarkers4s);
+        //Debug.Log("4x : " + gameModeRequiresShotMarkers4s);
+        //Debug.Log("app version : " + applicationVersion);
+        //Debug.Log("date  : " + DateTime.Now);
+        //Debug.Log("os  : " + operatingSystemVersion);
+    }
+}
---
+++ Assets/Scripts/scene_start/StartScreenPlayerSelected.cs
@@ -0,0 +1,156 @@
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class StartScreenPlayerSelected : MonoBehaviour
+//{
+//    [SerializeField]
+//    public string playerDisplayName;
+//    [SerializeField]
+//    public string playerObjectName;
+
+//    [SerializeField]
+//    private Sprite playerPortrait;
+
+//    float accuracy2pt;
+//    float accuracy3pt;
+//    float accuracy4pt;
+//    [SerializeField]
+//    float jumpForce;
+//    float speed;
+//    float runSpeed;
+//    float hangTime;
+//    [SerializeField]
+//    float range;
+//    float release;
+
+//    [SerializeField]
+//    public float jumpStatFloor;
+//    [SerializeField]
+//    public float jumpStatCeiling;
+
+//    public float criticalPercent;
+//    [SerializeField]
+//    private shooterProfile shooterProfile;
+//    [SerializeField]
+//    private GameObject shooterProfileObject;
+//    private string shooterProfilePrefabName;
+
+//    void Awake()
+//    {
+//       //Debug.Log("StartScreenPlayerSelected");
+//        shooterProfilePrefabName = "player"  + playerDisplayName;
+
+//       //Debug.Log(shooterProfilePrefabName);
+//        shooterProfileObject = Resources.Load("Prefabs /characters/"+shooterProfilePrefabName) as GameObject;
+//        Instantiate(shooterProfileObject);
+
+//        //basketball = Resources.Load("Prefabs/objects/basketball_nba") as GameObject;
+//        intializeShooterStatsFromProfile();
+//    }
+
+//    private void intializeShooterStatsFromProfile()
+//    {
+//       //Debug.Log("initializeStats()");
+//        Accuracy2Pt = shooterProfile.Accuracy2pt;
+//        Accuracy3Pt = shooterProfile.Accuracy3pt;
+//        Accuracy4Pt = shooterProfile.Accuracy4pt;
+//        JumpForce = shooterProfile.JumpForce;
+//       //Debug.Log(shooterProfile.JumpForce);
+//        CriticalPercent = shooterProfile.criticalPercent;
+
+//    }
+
+//    public float  calculateJumpValueToPercent()
+//    {
+
+//        //modifier
+//        float modifier = 100 /((jumpStatCeiling - jumpStatFloor) * 10);
+//        // percent
+//        float percent = (JumpForce - jumpStatFloor) * modifier *10;
+
+//        //Debug.Log(jumpStatCeiling - jumpStatFloor);
+//        //Debug.Log(" modifier : "+ modifier + "      percent : "+ percent);
+
+//        return percent;
+//    }
+
+//    public string PlayerObjectName
+//    {
+//        get => playerObjectName;
+//        set => playerObjectName = value;
+//    }
+
+//    public string PlayerName
+//    {
+//        get => playerDisplayName;
+//        set => playerDisplayName = value;
+//    }
+
+//    public Sprite PlayerPortrait
+//    {
+//        get => playerPortrait;
+//        set => playerPortrait = value;
+//    }
+
+//    public float Accuracy2Pt
+//    {
+//        get => accuracy2pt;
+//        set => accuracy2pt = value;
+//    }
+
+//    public float Accuracy3Pt
+//    {
+//        get => accuracy3pt;
+//        set => accuracy3pt = value;
+//    }
+
+//    public float Accuracy4Pt
+//    {
+//        get => accuracy4pt;
+//        set => accuracy4pt = value;
+//    }
+
+//    public float JumpForce
+//    {
+//        get => jumpForce;
+//        set => jumpForce = value;
+//    }
+
+//    public float Speed
+//    {
+//        get => speed;
+//        set => speed = value;
+//    }
+
+//    public float RunSpeed
+//    {
+//        get => runSpeed;
+//        set => runSpeed = value;
+//    }
+
+//    public float HangTime
+//    {
+//        get => hangTime;
+//        set => hangTime = value;
+//    }
+
+//    public float Range
+//    {
+//        get => range;
+//        set => range = value;
+//    }
+
+//    public float Release
+//    {
+//        get => release;
+//        set => release = value;
+//    }
+
+//    public float CriticalPercent
+//    {
+//        get => criticalPercent;
+//        set => criticalPercent = value;
+//    }
+//}
