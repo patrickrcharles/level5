19ea5199a32632d00d7cd58b865189a19593e2fb
Patrick Charles <patrickrcharles@gmail.com>
2020-08-10 15:30:57 -0500

    script for use with onscreen buttons
    keep for now, currently using gestures and taps for controls but this could be useful for UI nav

---
+++ Assets/Scripts/input/TouchButtonControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class TouchButtonControl : MonoBehaviour, IPointerDownHandler
+{
+    const string jumpButtonName = "jump";
+    const string shootButtonName = "shoot";
+    const string nextSceneButtonName = "nextScene";
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        //Debug.Log("button : " + eventData.button);
+        //Debug.Log("gname : " + gameObject.name);
+
+        if (gameObject.name.Equals(jumpButtonName))
+        {
+            GameLevelManager.Instance.PlayerState.TouchControlJump();
+        }
+        if (gameObject.name.Equals(shootButtonName))
+        {
+            GameLevelManager.Instance.PlayerState.touchControlShoot();
+        }
+        if (gameObject.name.Equals(nextSceneButtonName))
+        {
+
+            if ((SceneManager.GetActiveScene().buildIndex + 1) == SceneManager.sceneCountInBuildSettings)
+            {
+                SceneManager.LoadScene("level_01_scrapyard");
+            }
+            else
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+        }
+    }
+}
