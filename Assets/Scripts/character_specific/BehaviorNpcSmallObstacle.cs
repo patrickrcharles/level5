affc5835e9b57f394ed02c986058ea5aab6a9239
Patrick Charles <patrickrcharles@gmail.com>
2020-08-25 19:10:30 -0500

    rename file

--- Assets/Scripts/character_specific/BehaviorNpcSmallObstacle.cs
+++ Assets/Scripts/character_specific/BehaviorNpcSmallObstacle.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class BehaviorFlash : MonoBehaviour
+public class BehaviorNpcSmallObstacle : MonoBehaviour
 {
 
     AudioSource moonwalkAudio;
