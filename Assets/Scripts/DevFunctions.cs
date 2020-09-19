dcdaf2f9247f2bc0b2f454344c3b95d9338d57e1
Patrick Charles <patrickrcharles@gmail.com>
2020-09-19 12:56:54 -0500

    dev functions on gamemanger
    enable unlocks etc

---
+++ Assets/Scripts/DevFunctions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DevFunctions : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] CharacterProfile player;
+
+    private void Start()
+    {
+        player = GameLevelManager.instance.PlayerShooterProfile;
+    }
+
+
+    private void Update()
+    {
+        if(GameLevelManager.instance.Controls.Other.change.enabled 
+            && GameLevelManager.instance.Controls.Other.toggle_character_unlock.triggered)
+        {
+            Debug.Log(" unlock character stats");
+            player.Accuracy2Pt = 100;
+            player.Accuracy3Pt = 100;
+            player.Accuracy4Pt = 100;
+            player.Accuracy7Pt = 100;
+            player.Release = 100;
+            player.Range = 1000;
+            player.Luck = 10;
+        }
+    }
+}
