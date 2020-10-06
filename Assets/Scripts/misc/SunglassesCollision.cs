a375225eee7b2ec41354a805ee8de2dae36b6a93
Patrick Charles <patrickrcharles@gmail.com>
2020-10-06 12:41:16 -0500

    add post pocessing to they live + billboard pivot changes

--- Assets/Scripts/misc/SunglassesCollision.cs
+++ Assets/Scripts/misc/SunglassesCollision.cs
@@ -7,10 +7,13 @@ public class SunglassesCollision : MonoBehaviour
     // Start is called before the first frame update
     public SpriteRenderer spriteRenderer;
     bool sunglassesDisabled;
+    [SerializeField]
+    GameObject CameraPostProcessing;
 
     private void Start()
     {
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        CameraPostProcessing.SetActive(false);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -20,6 +23,7 @@ public class SunglassesCollision : MonoBehaviour
             sunglassesDisabled = true;
             TheyLiveManager.instance.TheyLiveEnabled = true;
             spriteRenderer.enabled = false;
+            CameraPostProcessing.SetActive(true);
         }
     }
 }
