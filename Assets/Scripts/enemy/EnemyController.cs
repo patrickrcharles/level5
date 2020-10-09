781b7242b5db93c4022e82b5325a0e8ab2adfc81
Patrick Charles <patrickrcharles@gmail.com>
2020-10-09 18:58:14 -0500

    packagemanger name space fix

--- Assets/Scripts/enemy/EnemyController.cs
+++ Assets/Scripts/enemy/EnemyController.cs
@@ -1,10 +1,7 @@
-﻿using System;
+﻿
 using System.Collections;
-using System.Collections.Generic;
 using Unity.Mathematics;
-using UnityEditor.PackageManager;
 using UnityEngine;
-using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
