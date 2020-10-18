c95dabff2a16880b441807708c65e924f0b66719
Patrick Charles <patrickrcharles@gmail.com>
2020-10-18 14:51:48 -0500

    animation renames + restructure + delete unsed sprite sheets

---
+++ Assets/Scripts/enemy/EnemyAnimationEvents.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAnimationEvents : MonoBehaviour
+{
+    [SerializeField]
+    GameObject attackBox;
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        if (GameLevelManager.instance.Basketball != null)
+        {
+            audioSource = GameObject.FindWithTag("basketball").GetComponent<AudioSource>();
+        }
+
+        attackBox = transform.parent.Find("attackBox").gameObject;
+        disableAttackBox();
+    }
+
+    public void enableAttackBox()
+    {
+        if (attackBox != null)
+        {
+            attackBox.SetActive(true);
+        }
+    }
+
+    public void disableAttackBox()
+    {
+        if (attackBox != null)
+        {
+            attackBox.SetActive(false);
+        }
+    }
+
+    public void playSfxBasketballDribbling()
+    {
+        audioSource.PlayOneShot(SFXBB.instance.basketballBounce);
+    }
+
+    public void playSfxAlienWalking()
+    {
+        audioSource.PlayOneShot(SFXBB.instance.alien_walk);
+    }
+
+    public void playSfxGameChanger()
+    {
+        audioSource.PlayOneShot(SFXBB.instance.gamechanger);
+    }
+
+    public void playSfxCameraFlash()
+    {
+        audioSource.PlayOneShot(SFXBB.instance.cameraFlash);
+    }
+
+    public void playSfxWerewolfHowl()
+    {
+        audioSource.PlayOneShot(SFXBB.instance.werewolfHowl);
+    }
+
+    public void playSfxWorkerParasite()
+    {
+        audioSource.PlayOneShot(SFXBB.instance.worker_parasite);
+    }
+
+    public void playSfxAirHorn()
+    {
+        audioSource.PlayOneShot(SFXBB.instance.airhorn);
+    }
+    public void playSfxLightningStrike()
+    {
+        audioSource.PlayOneShot(SFXBB.instance.lightningStrike);
+    }
+
+    public void playSfxRimShot()
+    {
+        audioSource.PlayOneShot(SFXBB.instance.rimShot);
+    }
+    public void playSfxKnockedDown()
+    {
+        audioSource.PlayOneShot(SFXBB.instance.knockedDown);
+    }
+
+    public void playSfxSkateGrind()
+    {
+        audioSource.PlayOneShot(SFXBB.instance.skateGrind);
+    }
+
+    public void playSfxGlitch()
+    {
+        audioSource.PlayOneShot(SFXBB.instance.glitch);
+    }
+
+    public void playSfxCloudOfSmoke()
+    {
+        audioSource.PlayOneShot(SFXBB.instance.turnIntoBat);
+    }
+
+    public void playSfxAirGuitar()
+    {
+        audioSource.PlayOneShot(SFXBB.instance.airGuitar);
+    }
+
+    public void playSfxChainRattle()
+    {
+        audioSource.PlayOneShot(SFXBB.instance.chainRattle);
+    }
+
+    public void playSfxDeathRay()
+    {
+        audioSource.PlayOneShot(SFXBB.instance.deathRay);
+    }
+
+    public void playSfxProbeDroidCritical()
+    {
+        audioSource.PlayOneShot(SFXBB.instance.probeCritical);
+    }
+    public void playSfxYell1()
+    {
+        audioSource.PlayOneShot(SFXBB.instance.yellYeah1, 0.3f);
+    }
+    public void playSfxYell2()
+    {
+        audioSource.PlayOneShot(SFXBB.instance.yellYeah2, 0.3f);
+    }
+    public void playSfxHitMetalBang()
+    {
+        audioSource.PlayOneShot(SFXBB.instance.metalBang);
+    }
+
+    public void playSfxStoneCold()
+    {
+        audioSource.PlayOneShot(SFXBB.instance.stoneCold);
+    }
+
+    public void playSfxChopWood()
+    {
+        audioSource.PlayOneShot(SFXBB.instance.chopWood);
+    }
+
+    public void playSfxShootGun()
+    {
+        audioSource.PlayOneShot(SFXBB.instance.shootGun);
+    }
+}
