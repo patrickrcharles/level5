08bc5d204b2f0bd004732f75846f340bff4ba11e
Patrick Charles <patrickrcharles@gmail.com>
2020-11-13 18:05:16 -0600

    adding fans system - very early and completely static right now

---
+++ Assets/Scripts/fans/FanManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanManager : MonoBehaviour
+{
+    GameObject basketBallGoalPosition;
+
+    private void Start()
+    {
+        // position transform relative to basketball goal
+        basketBallGoalPosition = GameObject.Find("rim");
+        //Debug.Log("vector : " + basketBallGoalPosition.transform.position);
+        transform.position = new Vector3(basketBallGoalPosition.transform.position.x, 0, basketBallGoalPosition.transform.position.z);
+        //Debug.Log("vector : " + position);
+        //transform.position = position;
+    }
+}
