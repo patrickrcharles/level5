9b733f9ae2090ef3faf2a05d91db286c601822e9
Patrick Charles <patrickrcharles@gmail.com>
2020-11-27 18:44:26 -0600

    health scripts and assets

---
+++ Assets/Scripts/enemy/EnemyHealth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField]
+    int health;
+    bool isDead;
+
+    public int Health { get => health; set => health = value; }
+
+    private void Start()
+    {
+        // default
+        health = 100;
+        if (health > 0)
+        {
+            isDead = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (health <= 0 && !isDead)
+        {
+            Debug.Log("enemy is dead");
+            isDead = true;
+        }
+    }
+}
---
+++ Assets/Scripts/player/PlayerHealth.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField]
+    int health;
+    [SerializeField]
+    int block;
+
+    bool isDead;
+
+    public int Health { get => health; set => health = value; }
+    public int Block { get => block; set => block = value; }
+
+    private void Start()
+    {
+        // default
+        health = 0;
+        block = 25;
+        if(health >= 100)
+        {
+            isDead = false;
+        }
+    }
+
+    private void Update()
+    {
+        if(health <= 0 && !isDead)
+        {
+            Debug.Log("player is dead");
+            isDead = true;
+        }
+    }
+}
