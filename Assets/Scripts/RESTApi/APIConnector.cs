7306ddcb6c5816dd525c829e37ede212599b14e7
Patrick Charles <patrickrcharles@gmail.com>
2021-01-14 13:31:09 -0600

    hiting api and desrializing data into an object
    booyah!

---
+++ Assets/Scripts/RESTApi/APIConnector.cs
@@ -0,0 +1,40 @@
+﻿
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using UnityEngine;
+
+public class APIConnector : MonoBehaviour
+{
+    const string publicApi = "http://13.58.224.237/api/";
+    const string publicApiHighScores = "http://13.58.224.237/api/highscores";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        getHighScores();
+    }
+
+
+    public void getHighScores()
+    {
+        HttpWebRequest request =
+            (HttpWebRequest)WebRequest.Create(String.Format(publicApiHighScores));
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+        StreamReader reader = new StreamReader(response.GetResponseStream());
+        //string jsonResponse = reader.ReadToEnd();
+        string jsonResponse = @"{""Highscores"":" + reader.ReadToEnd() + "}";
+        Debug.Log(jsonResponse);
+
+        APIHighScores highScores =  JsonConvert.DeserializeObject<APIHighScores>(jsonResponse);
+
+        Debug.Log(highScores.HighScores[1].Id);
+        Debug.Log(highScores.HighScores[1].UserId);
+        Debug.Log(highScores.HighScores[1].ModeId);
+      
+    }
+}
---
+++ Assets/Scripts/RESTApi/APIHighScores.cs
@@ -0,0 +1,28 @@
+﻿
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class APIHighScores 
+{
+    [JsonProperty("Id")]
+    public int Id;
+    [JsonProperty("UserId")]
+    public int UserId;
+    [JsonProperty("ModeId")]
+    public int ModeId;
+
+    public List<HighScore> HighScores;
+
+    [Serializable]
+    public class HighScore
+    {
+        [JsonProperty("Id")]
+        public int Id;
+        [JsonProperty("UserId")]
+        public int UserId;
+        [JsonProperty("ModeId")]
+        public int ModeId;
+    }
+}
