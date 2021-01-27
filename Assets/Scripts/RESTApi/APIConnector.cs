3727a584b1d53e74b20682fa200a2b938c9ce28f
Patrick Charles <patrickrcharles@gmail.com>
2021-01-26 18:47:49 -0600

    api post working + database model

--- Assets/Scripts/RESTApi/APIConnector.cs
+++ Assets/Scripts/RESTApi/APIConnector.cs
@@ -1,10 +1,12 @@
 ﻿
+using Assets.Scripts.database;
 using Newtonsoft.Json;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
+using System.Text;
 using UnityEngine;
 
 public class APIConnector : MonoBehaviour
@@ -15,41 +17,104 @@ public class APIConnector : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        getHighScores();
+        DBHighScoreModel dbs = gameObject.AddComponent<DBHighScoreModel>();
+        PostHighscore(dbs);
     }
 
-
-    public void getHighScores()
+    public void PostHighscore(DBHighScoreModel dbHighScoreModel)
     {
-        Debug.Log("hit API...");
-        HttpWebRequest request =
-            (HttpWebRequest)WebRequest.Create(String.Format(publicApiHighScores + "?modeid=1"));
-        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-        StreamReader reader = new StreamReader(response.GetResponseStream());
+        DBHighScoreModel dbHighScoreModel1 = new DBHighScoreModel();
+        string toJson = JsonUtility.ToJson(dbHighScoreModel1);
 
-        //Debug.Log("publicApiHighScores : " + publicApiHighScores +"/1");
 
-        string jsonResponse = @"{""Highscores"":" + reader.ReadToEnd() + "}";
-        Debug.Log(jsonResponse);
+        //string json =
+        //    "{ \"userid\": " + dbHighScoreModel.Userid + "," +
+        //    "\"modeid\": " + dbHighScoreModel.Modeid + "," +
+        //    "\"characterid\": " + dbHighScoreModel.Characterid + "," +
+        //    "\"levelid\": " + dbHighScoreModel.Levelid + "," +
+        //    "\"character\": '" + dbHighScoreModel.Character + "'," +
+        //    "\"level\": " + dbHighScoreModel.Level + "," +
+        //    "\"os\": " + SystemInfo.operatingSystem + "," +
+        //    "\"version\": " + Application.version + "," +
+        //    "\"date\": " + dbHighScoreModel.name + "," +
+        //    "\"time\": "+ dbHighScoreModel.Time +"," +
+        //    "\"totalPoints\": "+dbHighScoreModel.TotalPoints + "," +
+        //    "\"longestShot\": "+dbHighScoreModel.LongestShot +"," +
+        //    "\"totalDistance\": "+ dbHighScoreModel.TotalDistance +"," +
+        //    "\"maxShotMade\": "+ dbHighScoreModel.MaxShotMade + "," +
+        //    "\"maxShotAtt\": "+ dbHighScoreModel.MaxShotAtt +"," +
+        //    "\"consecutiveShots\": "+ dbHighScoreModel.ConsecutiveShots +"," +
+        //    "\"trafficEnabled\": "+ dbHighScoreModel.TrafficEnabled +"," +
+        //    "\"hardcoreEnabled\": "+ dbHighScoreModel.HardcoreEnabled +"," +
+        //    "\"enemiesKilled\": "+ dbHighScoreModel.EnemiesKilled +"," +
+        //    "\"platform\": "+ dbHighScoreModel.Platform +"," +
+        //     "\"device\": "+ dbHighScoreModel.Device +"," +
+        //     "\"ipaddress\": "+ dbHighScoreModel.Ipaddress +"," +
+        //     "\"scoreid\": "+ dbHighScoreModel.Scoreid +"," +
+        //     "\"twoMade\": "+ dbHighScoreModel.TwoMade +"," +
+        //     "\"twoAtt\": "+ dbHighScoreModel.TwoAtt +"," +
+        //     "\"threeMade\": "+ dbHighScoreModel.ThreeMade +"," +
+        //     "\"threeAtt\": "+ dbHighScoreModel.ThreeAtt +"," +
+        //     "\"fourMade\": "+ dbHighScoreModel.FourMade +"," +
+        //     "\"fourAtt\": "+ dbHighScoreModel.FourAtt +"," +
+        //     "\"sevenMade\": "+ dbHighScoreModel.SevenMade +"," +
+        //     "\"sevenAtt\": "+ dbHighScoreModel.SevenAtt +"}";
+        //Debug.Log(json);
+        Debug.Log("tojson : " + toJson);
 
-        APIHighScores highScores =  JsonConvert.DeserializeObject<APIHighScores>(jsonResponse);
 
-        int i = 0;
-        foreach(APIHighScores.HighScore score in highScores.HighScores) 
+        //ASCIIEncoding encoder = new ASCIIEncoding();
+        //byte[] data = encoder.GetBytes(serializedObject); // a json object, or xml, whatever...
+
+        var httpWebRequest = (HttpWebRequest)WebRequest.Create(publicApiHighScores) as HttpWebRequest;
+        httpWebRequest.ContentType = "application/json; charset=utf-8";
+        httpWebRequest.Method = "POST";
+
+        using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
         {
-            Debug.Log( i + "start ==============================================");
-            Debug.Log("id : " + score.Id);
-            Debug.Log("userid : " + score.UserId);
-            Debug.Log("modeid : " + score.ModeId);
-            Debug.Log("cid : " + score.CharacterId);
-            Debug.Log("lid : " + score.Level);
-            Debug.Log("lid : " + score.Character);
-            Debug.Log("lid : " + score.Date);
-            Debug.Log("lid : " + score.Device);
-            Debug.Log("lid : " + score.Platform);
-            Debug.Log(i + " end ==============================================");
-            i++;
+            string json = toJson;
+
+            streamWriter.Write(json);
+            streamWriter.Flush();
         }
-      
+
+        var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+        using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+        {
+            var result = streamReader.ReadToEnd();
+            Debug.Log(result);
+        }
     }
 }
+    //public void getHighScores()
+    //{
+    //    Debug.Log("hit API...");
+    //    HttpWebRequest request =
+    //        (HttpWebRequest)WebRequest.Create(String.Format(publicApiHighScores + "?modeid=1"));
+    //    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+    //    StreamReader reader = new StreamReader(response.GetResponseStream());
+
+    //    //Debug.Log("publicApiHighScores : " + publicApiHighScores +"/1");
+
+    //    string jsonResponse = @"{""Highscores"":" + reader.ReadToEnd() + "}";
+    //    Debug.Log(jsonResponse);
+
+    //    APIHighScores highScores =  JsonConvert.DeserializeObject<APIHighScores>(jsonResponse);
+
+    //    int i = 0;
+    //    foreach(APIHighScores.HighScore score in highScores.HighScores) 
+    //    {
+    //        Debug.Log( i + "start ==============================================");
+    //        Debug.Log("id : " + score.Id);
+    //        Debug.Log("userid : " + score.UserId);
+    //        Debug.Log("modeid : " + score.ModeId);
+    //        Debug.Log("cid : " + score.CharacterId);
+    //        Debug.Log("lid : " + score.Level);
+    //        Debug.Log("lid : " + score.Character);
+    //        Debug.Log("lid : " + score.Date);
+    //        Debug.Log("lid : " + score.Device);
+    //        Debug.Log("lid : " + score.Platform);
+    //        Debug.Log(i + " end ==============================================");
+    //        i++;
+    //    }   
+    //}
---
+++ Assets/Scripts/database/DBHighScoreModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.database
+{
+    [Serializable]
+    public class DBHighScoreModel : MonoBehaviour
+    {
+        public int Id;
+        public int Userid;
+        public int Modeid;
+        public int Characterid ;
+        public int Levelid ;
+        public string Character ;
+        public string Level ;
+        public string Os ;
+        public string Version ;
+        public string Date ;
+        public float Time ;
+        public int TotalPoints ;
+        public float LongestShot ;
+        public float TotalDistance ;
+        public int MaxShotMade ;
+        public int MaxShotAtt ;
+        public int ConsecutiveShots ;
+        public int TrafficEnabled ;
+        public int HardcoreEnabled ;
+        public int EnemiesKilled ;
+        public string Platform ;
+        public string Device ;
+        public string Ipaddress ;
+        public string Scoreid ;
+        public int TwoMade ;
+        public int TwoAtt ;
+        public int ThreeMade ;
+        public int ThreeAtt ;
+        public int FourMade ;
+        public int FourAtt ;
+        public int SevenMade ;
+        public int SevenAtt ;
+
+        //public int Id { get; set; }
+        //public int Userid { get; set; }
+        //public int Modeid { get; set; }
+        //public int Characterid { get; set; }
+        //public int Levelid { get; set; }
+        //public string Character { get; set; }
+        //public string Level { get; set; }
+        //public string Os { get; set; }
+        //public string Version { get; set; }
+        //public string Date { get; set; }
+        //public float Time { get; set; }
+        //public int TotalPoints { get; set; }
+        //public float LongestShot { get; set; }
+        //public float TotalDistance { get; set; }
+        //public int MaxShotMade { get; set; }
+        //public int MaxShotAtt { get; set; }
+        //public int ConsecutiveShots { get; set; }
+        //public int TrafficEnabled { get; set; }
+        //public int HardcoreEnabled { get; set; }
+        //public int EnemiesKilled { get; set; }
+        //public string Platform { get; set; }
+        //public string Device { get; set; }
+        //public string Ipaddress { get; set; }
+        //public string Scoreid { get; set; }
+        //public int TwoMade { get; set; }
+        //public int TwoAtt { get; set; }
+        //public int ThreeMade { get; set; }
+        //public int ThreeAtt { get; set; }
+        //public int FourMade { get; set; }
+        //public int FourAtt { get; set; }
+        //public int SevenMade { get; set; }
+        //public int SevenAtt { get; set; }
+    }
+}
