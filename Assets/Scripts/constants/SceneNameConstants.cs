d334e42f985ef298d4f33711f20e60449f09edec
Patrick Charles <patrickrcharles@gmail.com>
2021-02-26 14:33:20 -0600

    new static file for holding constants

---
+++ Assets/Scripts/constants/SceneNameConstants.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SceneNameConstants 
+{
+    public const string SCENE_NAME_level_00_account = "level_00_account";
+    public const string SCENE_NAME_level_00_credits = "level_00_credits";
+    public const string SCENE_NAME_level_00_loading = "level_00_laoding";
+    public const string SCENE_NAME_level_00_login = "level_00_login";
+    public const string SCENE_NAME_level_00_options = "level_00_options";
+    public const string SCENE_NAME_level_00_progression = "level_00_progression";
+    public const string SCENE_NAME_level_00_start = "level_00_start";
+    public const string SCENE_NAME_level_00_stats = "level_00_stats";
+    public const string SCENE_NAME_level_01_scrapyard = "level_01_scrapyard";
+    public const string SCENE_NAME_level_02_circlek = "level_02_circlek";
+    public const string SCENE_NAME_level_03_snow = "level_03_snow";
+    public const string SCENE_NAME_level_04_slab = "level_04_slab";
+    public const string SCENE_NAME_level_05_aveb = "level_05_aveb";
+    public const string SCENE_NAME_level_06_caffe = "level_06_caffe";
+    public const string SCENE_NAME_level_07_sudan = "level_07_sudan";
+    public const string SCENE_NAME_level_08_tammys = "level_08_tammys";
+    public const string SCENE_NAME_level_09_party_mansion = "level_09_party_mansion";
+    public const string SCENE_NAME_level_10_time_jail = "level_10_time_jail";
+    public const string SCENE_NAME_level_11_forest = "level_11_forest";
+    public const string SCENE_NAME_level_12_theater = "level_12_theater";
+    public const string SCENE_NAME_level_13_rustys = "level_13_rustys";
+    public const string SCENE_NAME_level_14_dome = "level_14_dome";
+
+}
