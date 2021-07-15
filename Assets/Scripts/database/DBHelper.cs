19ea6a295e4fd5de2561d0f39a78e5c0fc29f717
Patrick Charles <patrickrcharles@gmail.com>
2021-07-15 14:02:56 -0500

    Update DBHelper.cs

--- Assets/Scripts/database/DBHelper.cs
+++ Assets/Scripts/database/DBHelper.cs
@@ -240,7 +240,7 @@ public class DBHelper : MonoBehaviour
             int currentLevel = ((int)((PlayerData.instance.CurrentExperience + expGained) / 3000));
 
             // gained a level
-            if (currentLevel > prevLevel)
+            if(currentLevel > prevLevel)
             {
                 PlayerData.instance.UpdatePointsAvailable++;
             }
@@ -265,8 +265,6 @@ public class DBHelper : MonoBehaviour
                + ", level = " + currentLevel
                + ", pointsAvailable = " + updatePointsAvailable
                + " WHERE charid = " + GameOptions.characterId;
-
-            //Debug.Log(sqlQuery1);
 
             dbcmd.CommandText = sqlQuery1;
             IDataReader reader = dbcmd.ExecuteReader();
@@ -590,11 +588,7 @@ public class DBHelper : MonoBehaviour
                 //CharacterProfile temp = new CharacterProfile();
                 while (reader.Read())
                 {
-                    //CharacterProfile temp = null;
-                    //CharacterProfile temp = new CharacterProfile();
                     CharacterProfile temp = gameObject.AddComponent<CharacterProfile>();
-
-                    //CharacterProfile temp = gameObject.AddComponent<CharacterProfile>();
 
                     temp.PlayerId = reader.GetInt32(0);
                     temp.PlayerDisplayName = reader.GetString(1);
@@ -617,6 +611,12 @@ public class DBHelper : MonoBehaviour
                     temp.Release = reader.GetInt32(18);
                     temp.IsLocked = Convert.ToBoolean(reader.GetValue(19));
                     characterStats.Add(temp);
+
+                    //Debug.Log("-------------------------------------------------------------------------------");
+                    //Debug.Log(temp.PlayerDisplayName + " PointsAvailable : " + temp.PointsAvailable);
+                    //Debug.Log(temp.PointsUsed + " PointsUsed : " + temp.PointsUsed);
+                    //Debug.Log(temp.Level + " level : " + temp.Level);
+                    //Debug.Log(temp.Experience + " experience : " + temp.Experience);
 
                     Destroy(temp);
                 }
