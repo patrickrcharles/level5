d2fa2f028cfb97005d219c7fb9b0b017b3913e1f
Patrick Charles <patrickrcharles@gmail.com>
2021-07-15 22:39:22 -0500

    racing - cinderblock

--- Assets/Scripts/player_racing/RacingCinderBlock.cs
+++ Assets/Scripts/player_racing/RacingCinderBlock.cs
@@ -18,44 +18,86 @@ public class RacingCinderBlock : MonoBehaviour
     float defaultMovementSpeed;
     [SerializeField]
     Rigidbody rigidbody;
+    [SerializeField]
+    bool targetReached;
+    [SerializeField]
+    bool lockSwitch = false;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
         target = RacingGameManager.instance.Player.transform.position;
-        Debug.Log("RacingGameManager.instance.CharacterProfile.MaxSpeed : " + RacingGameManager.instance.CharacterProfile.MaxSpeed);
+        //Debug.Log("RacingGameManager.instance.CharacterProfile.MaxSpeed : " + RacingGameManager.instance.CharacterProfile.MaxSpeed);
         maxSpeed = RacingGameManager.instance.CharacterProfile.MaxSpeed * 2f;
         movementSpeed = defaultMovementSpeed;
 
-        InvokeRepeating("pursuePlayer", 0, 0.4f);
+        //InvokeRepeating("pursuePlayer", 0, 0.4f);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (movementSpeed < maxSpeed)
-        {
-            //movementSpeed = 1 + (acceleration / 100);
-            movementSpeed = (movementSpeed + (acceleration / 100));
-        }
+        pursuePlayer();
 
-        //pursuePlayer();
-        if (!RacingGameManager.instance.PlayerController.KnockedDown)
+        if (!targetReached)
         {
-            movement = target * (movementSpeed * Time.fixedDeltaTime);
-            //movement = targetPosition * (movementSpeed * Time.deltaTime);
-            rigidbody.MovePosition(transform.position + movement);
+            if (movementSpeed < maxSpeed)
+            {
+                //movementSpeed = 1 + (acceleration / 100);
+                movementSpeed = (movementSpeed + (acceleration / 100));
+            }
+
+            //pursuePlayer();
+            if (!RacingGameManager.instance.PlayerController.KnockedDown)
+            {
+                movement = target * (movementSpeed * Time.fixedDeltaTime);
+                //movement = targetPosition * (movementSpeed * Time.deltaTime);
+                rigidbody.MovePosition(transform.position + movement);
+            }
+            else
+            {
+                movementSpeed = 0;
+            }
         }
-        else
+        if (targetReached && !lockSwitch)
         {
-            movementSpeed = 0;
+            //transform.position = new Vector3(RacingGameManager.instance.Player.transform.position.x,
+            //    RacingGameManager.instance.Player.transform.position.y + 2,
+            //    RacingGameManager.instance.Player.transform.position.z);
+            lockSwitch = true;
+            Vector3 force = new Vector3(0, -20, 0);
+            rigidbody.AddForce(force, ForceMode.VelocityChange);
         }
     }
 
     public void pursuePlayer()
     {
-        target = (RacingGameManager.instance.Player.transform.position - transform.position).normalized;
+        if (!targetReached)
+        {
+            Vector3 newVector = new Vector3(RacingGameManager.instance.Player.transform.position.x+1,
+                RacingGameManager.instance.Player.transform.position.y + 2.5f,
+                RacingGameManager.instance.Player.transform.position.z);
+            target = (newVector - transform.position).normalized;
+            if((newVector.x - transform.position.x) < 1)
+            {
+                targetReached = true;
+            }
+        }
+        //if()
+        //if (targetReached)
+        //{
+        //    //Vector3 newVector = new Vector3(RacingGameManager.instance.Player.transform.position.x,
+        //    //    RacingGameManager.instance.Player.transform.position.y + 2,
+        //    //    RacingGameManager.instance.Player.transform.position.z);
+        //    //transform.position = new Vector3(RacingGameManager.instance.Player.transform.position.x,
+        //    //    RacingGameManager.instance.Player.transform.position.y + 2,
+        //    //    RacingGameManager.instance.Player.transform.position.z);
+        //    Vector3 force = new Vector3(0, -20, 0);
+        //    rigidbody.AddForce(force, ForceMode.VelocityChange);
+        //    //target = (newVector).normalized;
+        //    //Destroy(gameObject, 1);
+        //}
 
         //transform.Translate(movement);
 
@@ -65,9 +107,25 @@ public class RacingCinderBlock : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player") && gameObject.CompareTag("obstacle"))
+        //if (other.CompareTag("Player") && gameObject.CompareTag("obstacle") && !targetReached)
+        //{
+        //    targetReached = true;
+        //    Debug.Log("player tag");
+        //    //movementSpeed = defaultMovementSpeed;
+        //}
+        if ((other.CompareTag("ground") || other.CompareTag("playerHitbox")) && lockSwitch)
         {
-            movementSpeed = defaultMovementSpeed;
+            //lockSwitch = false;
+            rigidbody.detectCollisions = false;
+            Debug.Log("ground/playerHitbox tag");
+            //targetReached = true;
+            Vector3 instantiateVector = new Vector3(RacingGameManager.instance.Player.transform.position.x-35,
+                RacingGameManager.instance.Player.transform.position.y + 2,
+                RacingGameManager.instance.Player.transform.position.z);
+            Instantiate(RacingGameManager.instance.CinderBlockPrefab, instantiateVector, Quaternion.identity);
+
+            Destroy(this.gameObject);
+            //movementSpeed = defaultMovementSpeed;
         }
     }
 }
