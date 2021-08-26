b64797979562730bc882a2ace23ad2c5f67e453f
Patrick Charles <patrickrcharles@gmail.com>
2021-08-26 18:00:52 -0500

    sqlite assembly changes

--- Assets/Scripts/Utility/UtilityFunctions.cs
+++ Assets/Scripts/Utility/UtilityFunctions.cs
@@ -9,46 +9,59 @@ namespace Assets.Scripts.Utility
 {
     public static class UtilityFunctions
     {
+        //public static bool IsValidEmail(string email)
+        //{
+        //    if (string.IsNullOrWhiteSpace(email))
+        //    {
+        //        return false;
+        //    }
+        //    try
+        //    {
+        //        // Normalize the domain
+        //        email = Regex.Replace(email, @"(@)(.+)$", DomainMapper,
+        //                              RegexOptions.None, TimeSpan.FromMilliseconds(200));
+
+        //        // Examines the domain part of the email and normalizes it.
+        //        string DomainMapper(Match match)
+        //        {
+        //            // Use IdnMapping class to convert Unicode domain names.
+        //            var idn = new IdnMapping();
+
+        //            // Pull out and process domain name (throws ArgumentException on invalid)
+        //            string domainName = idn.GetAscii(match.Groups[2].Value);
+
+        //            return match.Groups[1].Value + domainName;
+        //        }
+        //    }
+        //    catch (RegexMatchTimeoutException)
+        //    {
+        //        return false;
+        //    }
+        //    catch (ArgumentException)
+        //    {
+        //        return false;
+        //    }
+
+        //    try
+        //    {
+        //        return Regex.IsMatch(email,
+        //            @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+        //            RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+        //    }
+        //    catch (RegexMatchTimeoutException)
+        //    {
+        //        return false;
+        //    }
+        //}
+
         public static bool IsValidEmail(string email)
         {
-            if (string.IsNullOrWhiteSpace(email))
-            {
-                return false;
-            }
             try
             {
-                // Normalize the domain
-                email = Regex.Replace(email, @"(@)(.+)$", DomainMapper,
-                                      RegexOptions.None, TimeSpan.FromMilliseconds(200));
-
-                // Examines the domain part of the email and normalizes it.
-                string DomainMapper(Match match)
-                {
-                    // Use IdnMapping class to convert Unicode domain names.
-                    var idn = new IdnMapping();
-
-                    // Pull out and process domain name (throws ArgumentException on invalid)
-                    string domainName = idn.GetAscii(match.Groups[2].Value);
-
-                    return match.Groups[1].Value + domainName;
-                }
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
             }
-            catch (RegexMatchTimeoutException)
-            {
-                return false;
-            }
-            catch (ArgumentException)
-            {
-                return false;
-            }
-
-            try
-            {
-                return Regex.IsMatch(email,
-                    @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
-                    RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
-            }
-            catch (RegexMatchTimeoutException)
+            catch
             {
                 return false;
             }
