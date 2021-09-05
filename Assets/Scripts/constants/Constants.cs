7e514a092e2894cc3274e7c4ccb192c0e200065d
Patrick Charles <patrickrcharles@gmail.com>
2021-09-05 16:06:04 -0500

    Update Constants.cs

--- Assets/Scripts/constants/Constants.cs
+++ Assets/Scripts/constants/Constants.cs
@@ -28,6 +28,7 @@ public static class Constants
     public const string SCENE_NAME_level_13_rustys = "level_13_rustys";
     public const string SCENE_NAME_level_14_dome = "level_14_dome";
     public const string SCENE_NAME_level_15_cocaine_island = "level_15_cocaine_island";
+    public const string SCENE_NAME_level_16_boner_mountain = "level_16_boner_mountain";
 
     // dev server api address constants
     public const string API_ADDRESS_DEV_publicApi = "http://13.58.224.237/api/";
