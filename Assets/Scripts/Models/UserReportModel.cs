34888192f1965e1a3b3874272f08899c221f189c
Patrick Charles <patrickrcharles@gmail.com>
2021-09-16 16:18:40 -0500

    user report models

---
+++ Assets/Scripts/Models/UserReportModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Models
+{
+    public class UserReportModel
+    {
+        public int Id ;
+        public int UserId ;
+        public string UserName ;
+        public string Report ;
+        public string IpAddress ;
+        public DateTime Date ;
+    }
+}
