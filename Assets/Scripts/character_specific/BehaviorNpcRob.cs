240e22b44522da7173966c6c06a861b6b501784e
Patrick Charles <patrickrcharles@gmail.com>
2021-10-17 00:01:27 -0500

    add rob lightning strike

--- Assets/Scripts/character_specific/BehaviorNpcRob.cs
+++ Assets/Scripts/character_specific/BehaviorNpcRob.cs
@@ -3,15 +3,10 @@
 public class BehaviorNpcRob : MonoBehaviour
 {
     [SerializeField] GameObject[] enemies;
-    // Start is called before the first frame update
-    [SerializeField]
-    Animator anim;
-
     private AudioSource audioSource;
 
     private void Start()
     {
-        anim = gameObject.GetComponent<Animator>();
         enemies = GameObject.FindGameObjectsWithTag("enemy");
         if (GameLevelManager.instance.Basketball != null)
         {
@@ -21,25 +16,18 @@ public class BehaviorNpcRob : MonoBehaviour
 
     private void LightningStrike()
     {
-        //enemies = GameObject.FindGameObjectsWithTag("enemy");
-        //Debug.Log("tets lighting animation");
-        //GameObject[] enemies = GameObject.FindGameObjectsWithTag("enemy");
         foreach (GameObject enemy in enemies)
         {
-            EnemyController enemyController = enemy.GetComponent<EnemyController>();
-            //enemyController.playAnimation("lightning");
-            //enemyController.playAnimation("lightning");
-            //enemyController.playAnimation("enemy_generic_lightning");
-            if (enemyController.SpriteRenderer.isVisible)
+            if (enemy != null)
             {
-                //StartCoroutine(enemyController.struckByLighning());
-                StartCoroutine(enemyController.struckByLighning());
+                EnemyController enemyController = enemy.GetComponent<EnemyController>();
+                if (enemyController.SpriteRenderer.isVisible)
+                {
+                    StartCoroutine(enemyController.struckByLighning());
+                }
             }
-            //enemyController.GetComponentInChildren<Animator>().Play("enemy_generic_lightning");
-            //Debug.Log("test lighting animation");
         }
     }
-
     private void DestroyRob()
     {
         Destroy(transform.root.gameObject);
