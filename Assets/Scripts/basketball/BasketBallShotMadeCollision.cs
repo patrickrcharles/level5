66de66f4b96ac642f3029bfcb4a5ded05760fa9b
Patrick Charles <patrickrcharles@gmail.com>
2023-07-31 09:26:44 -0500

    add bball collisioin class

---
+++ Assets/Scripts/basketball/BasketBallShotMadeCollision.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+
+public class BasketBallShotMadeCollision : MonoBehaviour
+{
+    [SerializeField]
+    BasketBallShotMade basketBallShotMade;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if ((other.gameObject.CompareTag("basketball") || other.gameObject.CompareTag("basketballAuto"))/*&& (!playerState.hasBasketball || !autoPlayerState.hasBasketball) */
+            //&& !isColliding
+            && gameObject.name.Equals("basketBallMadeShot2")
+            && basketBallShotMade.ShotMade1)
+        {
+            PlayerIdentifier pi = other.GetComponent<PlayerIdentifier>();
+            basketBallShotMade.ShotMade2 = true;
+            basketBallShotMade.shotMade(pi.GetComponent<GameStats>(), pi);
+        }
+    }
+}
+
