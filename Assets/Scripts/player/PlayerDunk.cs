15bac88cc2c46e91878df141641584a805039582
Patrick Charles <patrickrcharles@gmail.com>
2023-07-31 09:24:49 -0500

    Update PlayerDunk.cs

--- Assets/Scripts/player/PlayerDunk.cs
+++ Assets/Scripts/player/PlayerDunk.cs
@@ -5,30 +5,16 @@ using UnityEngine;
 public class PlayerDunk : MonoBehaviour
 {
     PlayerController playerController;
-
+    BasketBallState basketBallState;
+    BasketBall basketBall;
     private Vector3 dunkPositionLeft;
     private Vector3 dunkPositionRight;
     [SerializeField]
     private float dunkRangeFeet;
     [SerializeField]
     private float jumpAngle;
-
-    [SerializeField]
     private bool playerCanDunk;
 
-    //public static PlayerDunk instance;
-
-    private void Awake()
-    {
-        //if (instance == null)
-        //{
-        //    instance = this;
-        //}
-        //else
-        //{
-        //    Destroy(this);
-        //}
-    }
     private void Start()
     {
         if (GameObject.Find("basketball_goal") != null)
@@ -36,7 +22,10 @@ public class PlayerDunk : MonoBehaviour
             dunkPositionLeft = GameObject.Find("dunk_position_left").transform.position;
             dunkPositionRight = GameObject.Find("dunk_position_right").transform.position;
         }
-        playerController = GameLevelManager.instance.PlayerController1;
+        PlayerIdentifier pi = GetComponent<PlayerIdentifier>();
+        playerController = pi.playerController;
+        basketBall = pi.basketBallController;
+        basketBallState = pi.basketBallState;
         // default dunk values
         jumpAngle = 45;
         dunkRangeFeet = 15;
@@ -47,20 +36,20 @@ public class PlayerDunk : MonoBehaviour
     public void playerDunk()
     {
         playerController.CallBallToPlayer.Locked = true;
-        BasketBall.instance.BasketBallState.Locked = true;
+        basketBallState.Locked = true;
         playerController.CheckIsPlayerFacingGoal(); // turns player facing rim
 
         float bballRelativePositioning = GameLevelManager.instance.BasketballRimVector.x - transform.position.x;
         // shot type for stats
-        BasketBall.instance.updateBasketBallStateShotTypeOnShoot(BasketBallState.instance.TwoPoints,
-            BasketBallState.instance.ThreePoints,
-            BasketBallState.instance.FourPoints,
-            BasketBallState.instance.SevenPoints);
+        basketBall.updateBasketBallStateShotTypeOnShoot(basketBallState.TwoPoints,
+            basketBallState.ThreePoints,
+            basketBallState.FourPoints,
+            basketBallState.SevenPoints);
         //calculate shot distance 
-        Vector3 target = BasketBall.instance.BasketBallState.BasketBallTarget.transform.position;
+        Vector3 target = basketBallState.BasketBallTarget.transform.position;
         Vector3 tempPos = new Vector3(target.x, 0, target.z);
-        float tempDist = Vector3.Distance(tempPos, BasketBall.instance.BasketBallPosition.transform.position);
-        BasketBall.instance.LastShotDistance = tempDist;
+        float tempDist = Vector3.Distance(tempPos, basketBall.BasketBallPosition.transform.position);
+        basketBall.LastShotDistance = tempDist;
 
         // determine which side to dunk on
         if (bballRelativePositioning > 0 && !playerController.Locked)
@@ -83,13 +72,13 @@ public class PlayerDunk : MonoBehaviour
         yield return new WaitUntil(() => playerController.CurrentState == playerController.dunkState);
         yield return new WaitUntil(() => playerController.currentState != playerController.dunkState);
 
-        BasketBall.instance.BasketBallState.Thrown = true;
+        basketBallState.Thrown = true;
         playerController.UnFreezePlayerPosition();
 
         // move ball above rim
-        Vector3 temp = BasketBall.instance.BasketBallState.BasketBallTarget.transform.position;
-        BasketBall.instance.Rigidbody.velocity = Vector3.zero;
-        BasketBall.instance.transform.position = new Vector3(temp.x, temp.y, temp.z);
+        Vector3 temp = basketBallState.BasketBallTarget.transform.position;
+        basketBall.Rigidbody.velocity = Vector3.zero;
+        basketBall.transform.position = new Vector3(temp.x, temp.y, temp.z);
         //reset
         playerController.hasBasketball = false;
         playerController.SetPlayerAnim("hasBasketball", false);
