e911ffb977f50e66fe66a8e38618ea6f1bb16148
Patrick Charles <patrickrcharles@gmail.com>
2023-08-20 07:32:03 -0500

    Update BasketballState.cs

--- Assets/Scripts/basketball/BasketballState.cs
+++ Assets/Scripts/basketball/BasketballState.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 
 
 public class BasketBallState : MonoBehaviour
@@ -28,62 +30,58 @@ public class BasketBallState : MonoBehaviour
 
     private bool _playerOnMarker;
     private bool _playerOnMarkerOnShoot;
-    private bool __moneyBallEnabledOnShoot;
+    private bool _moneyBallEnabledOnShoot;
 
     private int _currentShotMarkerId;
     private int _onShootShotMarkerId;
 
     [SerializeField]
     private float _playerDistanceFromRim;
+    [SerializeField]
+    public bool isCpu;
+    [SerializeField]
+    GameObject player;
 
     private GameObject _basketBallPosition;
     private GameObject _basketBallTarget;
-       [SerializeField]
+    [SerializeField]
     private int _currentShotType;
+    private int _currentShotMade;
+
     public int CurrentShotType => _currentShotType;
-    public static BasketBallState instance;
+    //public static BasketBallState instance;
 
     private void Awake()
     {
-        instance = this;
+        //instance = this;
     }
 
     void Start()
     {
-
-        switch (GameLevelManager.instance.IsAutoPlayer)
+        isCpu = GetComponent<PlayerIdentifier>().isCpu;
+        if (isCpu)
         {
-            case true:
-                _basketBallPosition = GameLevelManager.instance.AutoPlayer.transform.Find("basketBall_position").gameObject;
-                break;
-            case false:
-                _basketBallPosition = GameLevelManager.instance.Player1.transform.Find("basketBall_position").gameObject;
-                break;
+            player = GetComponent<PlayerIdentifier>().autoPlayer;
         }
-        //// position of basketball infront of player
-        //_basketBallPosition = GameLevelManager.instance.Player.transform.Find("basketBall_position").gameObject;
-
+        else
+        {
+            player = GetComponent<PlayerIdentifier>().player;
+        }
         //position to shoot basketball at (middle of rim)
         _basketBallTarget = GameObject.Find("basketBall_target");
 
     }
-
-    // Update is called once per frame
     void Update()
     {
-
-        PlayerDistanceFromRim = GameLevelManager.instance.IsAutoPlayer 
-            ? GameLevelManager.instance.AutoPlayerController.PlayerDistanceFromRim  
-            : GameLevelManager.instance.PlayerController1.PlayerDistanceFromRim;
-        //PlayerDistanceFromRim = Vector3.Distance(GameLevelManager.instance.Player.transform.position, _basketBallTarget.transform.position);
+        PlayerDistanceFromRim = Vector3.Distance(player.transform.position, _basketBallTarget.transform.position);
         //PlayerDistanceFromRim = Mathf.Abs( GameLevelManager.instance.Player.transform.position.z - _basketBallTarget.transform.position.z);
 
         // is player on  marker  +  is marker required for game mode
         if (GameRules.instance.PositionMarkersRequired)
         {
-            PlayerOnMarker = GameRules.instance.BasketBallShotMarkersList[CurrentShotMarkerId].PlayerOnMarker;
+            PlayerOnMarker = isCpu ?  GameRules.instance.BasketBallShotMarkersList[CurrentShotMarkerId].AutoPlayerOnMarker 
+                : GameRules.instance.BasketBallShotMarkersList[CurrentShotMarkerId].PlayerOnMarker;
         }
-
 
         if (PlayerDistanceFromRim < Constants.DISTANCE_3point)
         {
@@ -94,7 +92,6 @@ public class BasketBallState : MonoBehaviour
         {
             TwoPoints = false;
         }
-
         if (PlayerDistanceFromRim >= Constants.DISTANCE_3point && PlayerDistanceFromRim < Constants.DISTANCE_4point)
         {
             ThreePoints = true;
@@ -104,7 +101,6 @@ public class BasketBallState : MonoBehaviour
         {
             ThreePoints = false;
         }
-
         if (PlayerDistanceFromRim >= Constants.DISTANCE_4point && PlayerDistanceFromRim < Constants.DISTANCE_7point)
         {
             FourPoints = true;
@@ -126,32 +122,43 @@ public class BasketBallState : MonoBehaviour
         }
     }
 
-    //bool isPlayerOnMarker()
+    //public bool isConsecutiveShot(GameStats gameStats)
     //{
-    //    // is player on 3point marker  + game requires 3 pt markers
-    //    //if (GameRules.instance.GameModeRequiresShotMarkers3S)
-    //    //{
-    //    //    PlayerOnMarker = GameRules.instance.BasketBallShotMarkersList[CurrentShotMarkerId].PlayerOnMarker;
-    //    //    return true;
-    //    //}
+    //    // get current state of shots made/attempted
+    //    _currentShotMade = (int)gameStats.ShotMade;
+    //    _currentShotAttempts = (int)gameStats.ShotAttempt;
 
-    //    //// is player on 4p oint marker  + game requires 4 pt markers
-    //    //if (GameRules.instance.GameModeRequiresShotMarkers4S)
-    //    //{
-    //    //    PlayerOnMarker = GameRules.instance.BasketBallShotMarkers4ptList[CurrentShotMarkerId].PlayerOnMarker;
-    //    //    return true;
-    //    //}
-
-    //    return false;
+    //    // if current is == expected made/attempt, increment consecutive and not a 2 point shot
+    //    // 
+    //    if (_currentShotMade == _expectedShotMade
+    //        && _currentShotAttempts == _expectedShotAttempts
+    //        && !basketBallState.TwoAttempt)
+    //    {
+    //        _consecutiveShotsMade++;
+    //        // increment expected values for next shot
+    //        _expectedShotMade = _currentShotMade + 1;
+    //        _expectedShotAttempts = _currentShotAttempts + 1;
+    //    }
+    //    // else, not consecutive shot. get current, increment for next expected consecutive shot
+    //    else
+    //    {
+    //        _consecutiveShotsMade = 1;
+    //        // increment expected values for next shot
+    //        _expectedShotMade = _currentShotMade + 1;
+    //        _expectedShotAttempts = _currentShotAttempts + 1;
+    //    }
+    //    // if current consecutive greater than previous high consecutive
+    //    if (ConsecutiveShotsMade > gameStats.MostConsecutiveShots)
+    //    {
+    //        gameStats.MostConsecutiveShots = ConsecutiveShotsMade;
+    //    }
+    //    return true;
     //}
-
-
     public bool MoneyBallEnabledOnShoot
     {
-        get => __moneyBallEnabledOnShoot;
-        set => __moneyBallEnabledOnShoot = value;
+        get => _moneyBallEnabledOnShoot;
+        set => _moneyBallEnabledOnShoot = value;
     }
-
     public bool PlayerOnMarker
     {
         get => _playerOnMarker;
@@ -162,14 +169,11 @@ public class BasketBallState : MonoBehaviour
         get => _playerOnMarkerOnShoot;
         set => _playerOnMarkerOnShoot = value;
     }
-
-
     public int CurrentShotMarkerId
     {
         get => _currentShotMarkerId;
         set => _currentShotMarkerId = value;
     }
-
     public int OnShootShotMarkerId
     {
         get => _onShootShotMarkerId;
@@ -180,37 +184,31 @@ public class BasketBallState : MonoBehaviour
         get => _twoPoints;
         set => _twoPoints = value;
     }
-
     public bool ThreePoints
     {
         get => _threePoints;
         set => _threePoints = value;
     }
-
     public bool FourPoints
     {
         get => _fourPoints;
         set => _fourPoints = value;
     }
-
     public bool SevenPoints
     {
         get => _sevenPoints;
         set => _sevenPoints = value;
     }
-
     public bool TwoAttempt
     {
         get => _twoAttempt;
         set => _twoAttempt = value;
     }
-
     public bool ThreeAttempt
     {
         get => _threeAttempt;
         set => _threeAttempt = value;
     }
-
     public bool FourAttempt
     {
         get => _fourAttempt;
@@ -222,58 +220,50 @@ public class BasketBallState : MonoBehaviour
         get => _sevenAttempt;
         set => _sevenAttempt = value;
     }
-
     public bool Dunk
     {
         get => _dunk;
         set => _dunk = value;
     }
-
     public bool InAir
     {
         get => _inAir;
         set => _inAir = value;
     }
-
     public bool Thrown
     {
         get => _thrown;
         set => _thrown = value;
     }
-
     public bool Locked
     {
         get => _locked;
         set => _locked = value;
     }
-
     public bool CanPullBall
     {
         get => _canPullBall;
         set => _canPullBall = value;
     }
-
     public bool Grounded
     {
         get => _grounded;
         set => _grounded = value;
     }
-
     public float PlayerDistanceFromRim
     {
         get => _playerDistanceFromRim;
         set => _playerDistanceFromRim = value;
     }
-
     public GameObject BasketBallPosition
     {
         get => _basketBallPosition;
         set => _basketBallPosition = value;
     }
-
     public GameObject BasketBallTarget
     {
         get => _basketBallTarget;
         set => _basketBallTarget = value;
     }
+    public GameObject Player { get => player; set => player = value; }
 }
