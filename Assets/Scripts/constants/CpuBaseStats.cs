32b2a2758cff7e1ea35f887fc1e43ddbbeae856f
Patrick Charles <patrickrcharles@gmail.com>
2023-09-27 10:16:40 -0500

    Update CpuBaseStats.cs

--- Assets/Scripts/constants/CpuBaseStats.cs
+++ Assets/Scripts/constants/CpuBaseStats.cs
@@ -11,7 +11,7 @@ public static class CpuBaseStats
     public const  int LUCK = 0;
     public const  int CLUTCH = 0;
     public const  int CLUTCH_DIVIDER = 2;
-    public const  int LUCK_DIVIDER = 10;
+    public const  int LUCK_DIVIDER = 3;
     public const float JUMP_FLOOR = 3.5f;
     public const float JUMP_CEILING = 6;
     public const float SPEED_FLOOR = 2.5f;
