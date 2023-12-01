e7c3ee52127e117a9f2034247b169ade46608f12
Patrick Charles <patrickrcharles@gmail.com>
2023-11-30 20:26:20 -0600

    Update PlayerAttackPosition.cs

--- Assets/Scripts/player/PlayerAttackPosition.cs
+++ Assets/Scripts/player/PlayerAttackPosition.cs
@@ -21,19 +21,19 @@ public class PlayerAttackPosition : MonoBehaviour
             engaged = false;
         }
         Vector3 playerTransform = playerIdentifier.isCpu ? playerIdentifier.autoPlayer.transform.position : playerIdentifier.player.transform.position;
-        if (attackPositionId == 1)
+        if (attackPositionId == 0)
         {
             transform.position = new Vector3(playerTransform.x - 0.6f, playerTransform.y, playerTransform.z - 0.25f);
         }
-        if (attackPositionId == 2)
+        if (attackPositionId == 1)
         {
             transform.position = new Vector3(playerTransform.x - 0.6f, playerTransform.y, playerTransform.z + 0.25f);
         }
-        if (attackPositionId == 3)
+        if (attackPositionId == 2)
         {
             transform.position = new Vector3(playerTransform.x + 0.6f, playerTransform.y, playerTransform.z - 0.25f);
         }
-        if (attackPositionId == 4)
+        if (attackPositionId == 3)
         {
             transform.position = new Vector3(playerTransform.x + 0.6f, playerTransform.y, playerTransform.z + 0.25f);
         }
