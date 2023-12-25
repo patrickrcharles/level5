702aa720caf029cae7eaebed1eaff6dc4f0c9bf9
Patrick Charles <patrickrcharles@gmail.com>
2023-12-25 15:01:19 -0600

    Update GameRules.cs
    add level to display

--- Assets/Scripts/game manager/GameRules.cs
+++ Assets/Scripts/game manager/GameRules.cs
@@ -361,6 +361,7 @@ public class GameRules : MonoBehaviour
             playerType = players[0].isCpu ? "CPU" : "Player";
             if (!players[0].isCpu) { displayP1ScoreText.color = Color.green; } else { displayP1ScoreText.color = Color.white; }
             displayP1ScoreText.text = playerType + " " + (players[0].pid + 1)
+                + "\n" + "lvl : "+ players[0].characterProfile.Level
                 + "\n" + players[0].characterProfile.PlayerDisplayName
                 + "\n" + "points : " + players[0].gameStats.TotalPoints
                 + "\n" + players[0].gameStats.ShotMade + "/" + players[0].gameStats.ShotAttempt
@@ -371,6 +372,7 @@ public class GameRules : MonoBehaviour
             playerType = players[1].isCpu ? "CPU" : "Player";
             if (!players[1].isCpu) { displayP2ScoreText.color = Color.green; } else { displayP2ScoreText.color = Color.white; }
             displayP2ScoreText.text = playerType + " " + (players[1].pid + 1)
+                + "\n" + "lvl : " + players[1].characterProfile.Level
                 + "\n" + players[1].characterProfile.PlayerDisplayName
                 + "\n" + "points : " + players[1].gameStats.TotalPoints
                 + "\n" + players[1].gameStats.ShotMade + "/" + players[1].gameStats.ShotAttempt
@@ -385,6 +387,7 @@ public class GameRules : MonoBehaviour
             playerType = players[2].isCpu ? "CPU" : "Player";
             if (!players[2].isCpu) { displayP3ScoreText.color = Color.green; } else { displayP3ScoreText.color = Color.white; }
             displayP3ScoreText.text = playerType + " " + (players[2].pid + 1)
+                + "\n" + "lvl : "+ players[2].characterProfile.Level
                 + "\n" + players[2].characterProfile.PlayerDisplayName
                 + "\n" + "points : " + players[2].gameStats.TotalPoints
                 + "\n" + players[2].gameStats.ShotMade + "/" + players[2].gameStats.ShotAttempt
@@ -399,6 +402,7 @@ public class GameRules : MonoBehaviour
             playerType = players[3].isCpu ? "CPU" : "Player";
             if (!players[3].isCpu) { displayP4ScoreText.color = Color.green; } else { displayP4ScoreText.color = Color.white; }
             displayP4ScoreText.text = playerType + " " + (players[3].pid + 1)
+                + "\n" + "lvl : " + players[3].characterProfile.Level
                 + "\n" + players[3].characterProfile.PlayerDisplayName
                 + "\n" + "points : " + players[3].gameStats.TotalPoints
                 + "\n" + players[3].gameStats.ShotMade + "/" + players[3].gameStats.ShotAttempt
