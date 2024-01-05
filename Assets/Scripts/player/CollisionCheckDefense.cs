c29e555ead680055cfbb660ae55e56a4094e0149
Patrick Charles <patrickrcharles@gmail.com>
2024-01-04 21:41:09 -0600

    Create CollisionCheckDefense.cs

---
+++ Assets/Scripts/player/CollisionCheckDefense.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class CollisionCheckDefense : MonoBehaviour
+{
+    [SerializeField] AutoPlayerDefense autoPlayerDefense;
+    [SerializeField] bool isLocked;
+
+    private void Start()
+    {
+        autoPlayerDefense = GetComponentInParent<AutoPlayerDefense>();
+    }
+
+    private void Update()
+    {
+        if (autoPlayerDefense.grounded)
+        {
+            isLocked = false;
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("basketball") 
+            && gameObject.CompareTag("cpuDefenseBlockBox")
+            && !isLocked
+            && autoPlayerDefense.playerIdentifier.playerController.InAir)
+        {
+            isLocked = true;
+            autoPlayerDefense.blockedShots++;
+            Debug.Log("shot blocked");
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("basketball") 
+            && gameObject.CompareTag("cpuDefenseBlockBox"))
+        {
+            isLocked = false;
+        }
+    }
+}
