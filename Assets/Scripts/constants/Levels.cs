1b4db7bf1d18cd2dd9c0b876083fc74e4c08c574
Patrick Charles <patrickrcharles@gmail.com>
2024-02-08 20:21:16 -0600

    levels constant

---
+++ Assets/Scripts/constants/Levels.cs
@@ -0,0 +1,27 @@
+
+public static class Levels
+{
+    public const int Scrapyard = 1;
+    public const int CircleK = 2;
+    public const int AveB = 3;
+    public const int Norf = 4;
+    public const int Slab = 5;
+    public const int Caffe = 6;
+    public const int Sudan = 7;
+    public const int Tammys = 8;
+    public const int PartyMansion = 9;
+    public const int TimeJail = 10;
+    public const int Nexus = 11;
+    public const int Rustys = 13;
+    public const int Dome = 14;
+    public const int CocaineIsland = 15;
+    public const int BonerMountain = 16;
+    public const int RumblePitFighting = 17;
+    public const int CrankZoneFighting = 18;
+    public const int BackYard = 19;
+    public const int Jacksonville = 20;
+    public const int Seaside = 21;
+    public const int RumblePitShooting = 22;
+    public const int Dev = 23;
+    public const int AveBCemetary = 24;
+}
